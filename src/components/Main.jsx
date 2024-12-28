37c918c1042b30a2ee7bcf87ffc6b46d573ed223
syeduroojkamal <syeduroojkamal26@gmail.com>
2024-12-28 23:19:54 +0530

    iteration 6

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,26 +18,17 @@ export default function Main() {
         <h1>Skills</h1>
         <div className="skills">
           <span>MERN Stack</span>
-          <span>React JS</span>
-          <span>JavaScript</span>
-          <span>Node JS</span>
           <span>Mongo DB</span>
           <span>MySQL</span>
           <span>Express JS</span>
+          <span>React JS</span>
+          <span>Node JS</span>
+          <span>JavaScript</span>
+          <span>Cloud Computing</span>
+          <span>Linux</span>
           <span>Docker</span>
           <span>Terraform</span>
           <span>Git</span>
-          <span>Github</span>
-          <span>Linux</span>
-          <span>Cloud Computing</span>
-          <span>Responsive design</span>
-          <span>HTML5</span>
-          <span>CSS3</span>
-          <span>Performance Testing</span>
-          <span>JMeter</span>
-          <span>LoadRunner</span>
-          <span>JavaScript frameworks</span>
-          <span>cross-browser compatibility</span>
         </div>
       </div>
     </main>
